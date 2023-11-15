4c166a6c99a048de73b99d929e0dbcd0ae22abb6
mahsavahidi <vahidimahsa17@gmail.com>
2023-11-15 17:38:44 -0400

    adding all files

---
+++ about.js
@@ -0,0 +1,8 @@
+// function changePosition(){
+//     var oldPosition = document.getElementById("image").style.top;
+//     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+//     if (scrollTop > 50) {
+//         oldPosition = "60px"
+//      }
+// }
+ghf
---
+++ app.js
@@ -0,0 +1,1 @@
+buttonFunction = () => document.getElementById('h1').innerHTML = "hi";
