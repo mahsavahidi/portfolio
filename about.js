5dbf01aae5a0c55624ec78551652ddb7d70c9e6d
mahsavahidi <vahidimahsa17@gmail.com>
2023-12-15 12:15:27 -0400

    project html page added content

--- about.js
+++ about.js
@@ -1,8 +1,16 @@
-// function changePosition(){
-//     var oldPosition = document.getElementById("image").style.top;
-//     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-//     if (scrollTop > 50) {
-//         oldPosition = "60px"
-//      }
-// }
-ghf
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});}
